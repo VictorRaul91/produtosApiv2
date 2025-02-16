798a82cb6c434457979e6c1c3983c050f380ad85
VictorRaul91 <vraul91@gmail.com>
2025-02-16 16:50:22 -0300

    Add files via upload

---
+++ produtosApiv2/src/main/java/br/com/cotiinformatica/entities/Produto.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.entities;
+
+import java.util.UUID;
+
+public class Produto {
+	
+	private UUID id;
+	private String nome;
+	private Double preco;
+	private Integer quantidade;
+	private Categoria categoria;
+
+}
